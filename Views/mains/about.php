c313461d975fab3287fe19efb43e4858e149d67d
RazvanAlexe <34007818+RazvanAlexe@users.noreply.github.com>
2022-06-27 19:11:35 +0300

    Add files via upload

--- Views/mains/about.php
+++ Views/mains/about.php
@@ -1,26 +1,28 @@
-<div>
-    <img src="/dailyvir/Content/image/background_purp.png" alt="">
-</div>
-<div>
-    IT'S YOUR TIME
-</div>
-<div>
-    <h3 style="text-align: center;font-weight: 600; ">
-        ABOUT US</h3>
+<div class="aboutPage">
+    <div>
+        <img src="/dailyvir/Content/image/background_purp.png" alt="">
+    </div>
+    <div>
+        <label style="color: black;">IT'S YOUR TIME</label>
+    </div>
+    <div>
+        <h3 style="text-align: center;font-weight: 600; ">
+            ABOUT US</h3>
+        <label style="color: black;">       Our mission is to give everyone a voice and show them the world.
 
-
-    Our mission is to give everyone a voice and show them the world.
-
-    We believe that everyone deserves to have a voice,
-    and that the world is a better place when we listen,
-    share and build community through our stories.
-</div>
-<div>
-    <a href="/dailyvir/mains/development">Development document</a>
-</div>
-<div>
-    <a href="/dailyvir/mains/howto">"How to" document</a>
-</div>
-<div>
-    <a href="/dailyvir/mains/concept">Concept document</a>
+We believe that everyone deserves to have a voice,
+and that the world is a better place when we listen,
+share and build community through our stories.</label>
+    </div>
+    <div class="documents">
+        <div>
+            <a href="/dailyvir/mains/development">Development document</a>
+        </div>
+        <div>
+            <a href="/dailyvir/mains/howto">"How to" document</a>
+        </div>
+        <div>
+            <a href="/dailyvir/mains/concept">Concept document</a>
+        </div>
+    </div>
 </div>
