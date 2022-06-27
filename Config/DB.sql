2dfc61be585e1122ef0cf459a7016384f18f33b5
RazvanAlexe <34007818+RazvanAlexe@users.noreply.github.com>
2022-06-27 19:30:34 +0300

    Update DB.sql

--- Config/DB.sql
+++ Config/DB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 25, 2022 at 11:02 PM
+-- Generation Time: Jun 27, 2022 at 06:30 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.5
 
@@ -12,7 +12,7 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 --
--- Database: `users`
+-- Database: `dailyvirdb`
 --
 
 -- --------------------------------------------------------
@@ -35,8 +35,9 @@ CREATE TABLE `comments` (
 
 INSERT INTO `comments` (`id_comment`, `post_time`, `id_user`, `comment`, `id_video`) VALUES
 (6, '2022-06-25 18:40:21', 'DailyViR', 'We highly reccomend this!', '722602885'),
-(7, '2022-06-25 20:44:01', 'Razvan', 'I love this movie', '715841302'),
-(8, '2022-06-25 20:44:16', 'Andreea', 'I hate it!', '715841302');
+(46, '2022-06-26 12:33:46', 'admin', 'a', '722602885'),
+(47, '2022-06-26 12:34:51', 'admin', 'a', '722602885'),
+(56, '2022-06-27 11:48:12', 'DailyViR', 'I really like this new comment style!', '715841302');
 
 -- --------------------------------------------------------
 
@@ -56,9 +57,11 @@ CREATE TABLE `favourites` (
 
 INSERT INTO `favourites` (`id_user`, `id_video`, `title`) VALUES
 ('razvan', '690017366', '690017366'),
-('razvan', '722597163', 'Dupa ce promovezi la TW'),
 ('DailyViR', '722602885', 'The Tunnel'),
-('Razvan', '715841302', 'The Rabbit Hole');
+('Razvan', '715841302', 'The Rabbit Hole'),
+('admin', '722602885', 'The Tunnel'),
+('DailyViR', '715841302', 'The Rabbit Hole'),
+('admin', '715841302', 'The Rabbit Hole');
 
 -- --------------------------------------------------------
 
@@ -82,8 +85,17 @@ CREATE TABLE `statistcs` (
 --
 
 INSERT INTO `statistcs` (`id_video`, `views`, `male_views`, `female_views`, `binary_views`, `others_views`, `today_views`, `dateToday`) VALUES
-('722602885', 4, 1, 0, 0, 1, 0, '2022-06-25'),
-('715841302', 12, 6, 0, 0, 0, 0, '2022-06-25');
+('722602885', 73, 64, 0, 0, 7, 0, '2022-06-25'),
+('715841302', 378, 319, 0, 0, 47, 0, '2022-06-25'),
+('713742301', 4, 0, 0, 0, 4, 0, '2022-06-27'),
+('716651419', 0, 0, 0, 0, 0, 0, '2022-06-27'),
+('701954773', 2, 1, 0, 0, 1, 0, '2022-06-27'),
+('250959576', 11, 1, 0, 0, 10, 0, '2022-06-27'),
+('720569971', 0, 0, 0, 0, 0, 0, '2022-06-27'),
+('719182720', 1, 0, 0, 0, 1, 0, '2022-06-27'),
+('673215931', 2, 0, 0, 0, 2, 0, '2022-06-27'),
+('665179690', 1, 0, 0, 0, 1, 0, '2022-06-27'),
+('447067692', 5, 0, 0, 0, 5, 0, '2022-06-27');
 
 -- --------------------------------------------------------
 
@@ -109,8 +121,9 @@ INSERT INTO `users` (`username`, `gender`, `email`, `password`, `country`, `fav_
 ('Andreea', 'Female', 'andreea@gmail.com', 'parola', 'Austria', 0),
 ('andrei', 'Male', 'andrei@email.com', 'parola', 'America', 1),
 ('Catalina', 'Female', 'Catalina@email.com', 'parola1', 'Romania', 0),
-('DailyViR', 'Other', 'DailyViR@gmail.com', 'parola1', 'Romania', 0),
-('razvan', 'Male', 'razvan@email.com', 'aaaaa', 'Romania', 1);
+('DailyViR', 'Other', 'DailyViR@gmail.com', 'parolanoua', 'Romania', 0),
+('razvan', 'Male', 'razvan@email.com', 'aaaaa', 'Romania', 1),
+('rss', 'Other', 'rss@rss.com', 'rss', 'Albania', 0);
 
 -- --------------------------------------------------------
 
@@ -135,8 +148,17 @@ CREATE TABLE `video` (
 --
 
 INSERT INTO `video` (`id_video`, `comment_count`, `views`, `category`, `upload_date`, `tags`, `title`, `description`, `id_user`) VALUES
-('715841302', 2, 9, 'Movie', '2022-06-25 20:44:29', 'video', 'The Rabbit Hole', 'Two sisters are faced with the grim reality of what it takes to make their dream come true. (*This is a film depicting the realities of trafficking. Viewer discretion is advised.) ', 'Razvan'),
-('722602885', 1, 62, 'Movie', '2022-06-25 20:43:09', 'video', 'The Tunnel', 'Three refugees run the race of their lives from Calais to Dover through the Euro Tunnel, trying to beat the trains and overcome their terror in a bid to reach freedom and start new lives in the UK. Based on true accounts.', 'DailyViR');
+('250959576', 0, 11, 'Music', '2022-06-27 16:07:49', 'video', 'Ici, là et partout', 'Love for all humanity', 'DailyViR'),
+('447067692', 0, 5, 'Animation', '2022-06-27 13:21:04', 'video', 'Brontosaurus', 'Official Selection: True/False Film Festival Nashville Film Festival Florida Film Festival Philadelphia Film Festival Cucalorus Film Festival NoBudge', 'DailyViR'),
+('665179690', 0, 1, 'Music', '2022-06-27 13:00:45', 'video', 'KANYE WEST x GAP | Arnaud Bresson | DIVISION', 'Kanye West x Gap | Heaven & Hell • Directed by Arnaud Bresson • Produced by DIVISION • Post by Mikros MPC', 'DailyViR'),
+('673215931', 0, 2, 'Music', '2022-06-27 11:58:29', 'video', 'HUGO BOSS | Marco Prestini | DIVISION', 'HUGO BOSS | The Scent • Directed by Marco Prestini • Produced by DIVISION • Post by Nightshift • CGI by Machine Molle Studio', 'DailyViR'),
+('701954773', 0, 2, '', '2022-06-27 11:12:19', 'video', '', '', 'rss'),
+('713742301', 0, 4, '713742301', '2022-06-26 21:27:41', 'video', '713742301', '713742301', 'rss'),
+('715841302', 48, 375, 'Movie', '2022-06-27 16:07:52', 'video', 'The Rabbit Hole', 'Two sisters are faced with the grim reality of what it takes to make their dream come true. (*This is a film depicting the realities of trafficking. Viewer discretion is advised.) ', 'Razvan'),
+('716651419', 0, 0, '', '2022-06-26 21:22:21', 'video', '', '', 'rss'),
+('719182720', 0, 1, 'Music', '2022-06-27 15:04:45', 'video', 'PHARRELL WILLIAMS ft. 21 SAVAGE, TYLER, THE CREATOR | François Rousselet | DIVISION', 'PHARRELL WILLIAMS FT. 21 SAVAGE, TYLER, THE CREATOR | Cash In Cash Out • Directed by François Rousselet • Produced by DIVISION • VFX by ETC', 'DailyViR'),
+('720569971', 0, 0, 'Music', '2022-06-27 11:30:38', 'video', 'Video Matters - June Issue', 'A monthly selection of brilliant videos from across the Vimeo community.', 'DailyViR'),
+('722602885', 3, 131, 'Movie', '2022-06-27 15:18:39', 'video', 'The Tunnel', 'Three refugees run the race of their lives from Calais to Dover through the Euro Tunnel, trying to beat the trains and overcome their terror in a bid to reach freedom and start new lives in the UK. Based on true accounts.', 'DailyViR');
 
 --
 -- Indexes for dumped tables
@@ -174,7 +196,7 @@ ALTER TABLE `video`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id_comment` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_comment` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- Constraints for dumped tables
